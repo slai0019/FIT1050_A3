a86be5066a0ea97bdc415f0ad8d1d6d70ff8ced7
slai0019 <84922284+slai0019@users.noreply.github.com>
2021-05-28 01:23:44 +1000

    Create Club Web

---
+++ layout/scripts/addScript.js
@@ -0,0 +1,31 @@
+$('#rList li').hover(function () {
+    $(this).css("color", "orange")
+}, function () {
+    $(this).css('color', 'rgb(38, 82, 117)');
+});
+
+$('#listTitle').hover(function () {
+    $(this).css("color", "orange")
+}, function () {
+    $(this).css('color', 'rgb(38, 82, 117)');
+});
+
+$('#p1').hover(function () {
+    $(this).css("color", "orange")
+}, function () {
+    $(this).css('color', 'rgb(38, 82, 117)');
+});
+
+$('#vedio1').hover(function () {
+    $(this).css("color", "orange")
+}, function () {
+    $(this).css('color', 'rgb(38, 82, 117');
+});
+
+$('#vedio2').hover(function () {
+    $(this).css("color", "orange")
+}, function () {
+    $(this).css('color', 'rgb(38, 82, 117');
+});
+
+
